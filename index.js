9b04b3e340bdec6b8badc256b803f782dbee62f8
Mohammad-Ali A'RÂBI <aerabi@gmx.de>
2020-10-04 23:51:32 +0200

    Feat: Add unit tests and adapt config to pass them
    Tests for Commandments 1, 2, 3, 4, and 6 were added.
    
    Fixes #3

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 module.exports = {
     rules: {
         'header-max-length': [2, 'always', 72],
+        'header-min-length': [2, 'always', 1],
         'type-case': [2, 'always', 'pascal-case'],
         'type-enum': [2, 'always', ['Feat', 'Fix', 'Docs', 'Style', 'Refactor', 'Test']],
+        'type-empty': [1, 'never'],
         'scope-case': [2, 'always', 'pascal-case'],
         'subject-case': [2, 'always', 'sentence-case'],
         'subject-empty': [2, 'never'],
         'subject-full-stop': [2, 'never', '.'],
-        'body-leading-blank': [1, 'always'],
+        'body-leading-blank': [2, 'always'],
         'body-max-line-length': [2, 'always', 72],
         'footer-leading-blank': [1, 'always'],
         'footer-max-line-length': [2, 'always', 72],
